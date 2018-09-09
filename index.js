aafb42fd159ca5405bd52805dfe240c4c1c1356c
Vitor Marcelino <vitormarcelino93@gmail.com>
2018-09-09 19:04:33 -0300

    Ajust in skill content

--- index.js
+++ index.js
@@ -18,70 +18,81 @@ var helpMessage = "VITOR MARCELINO | BACKEND DEVELOPER \n \
 ...........................................................................................................\n \
     \n \
     Options: \n \
-    h or help : for list commands  \n \
-    s or skills : display my technology skills\n \
-    e or experience : display my experiences\n \
-    c or contact : display my contacts\n \
+    h, help : for list commands  \n \
+    s, skills : display my technology skills\n \
+    e, experience : display my experiences\n \
+    c, contact : display my contacts\n \
 ";
 
 $('body').terminal(function(command, term) {
     if (command == 'help' || command == 'h') {
         this.echo(helpMessage);
-    }
+    } else
     
     if (command == 'skills' || command == 's') {
         printColor("SO and Containers", 'green', this);
-        this.echo("Linux          * * * * -");
-        this.echo("Docker         * * * * -");
+        this.echo("Linux                    * * * * -");
+        this.echo("Docker                   * * * * -");
+        this.echo("MacOS                    * * * * -");
+        this.echo("Windows                  * * * * -");
         
         printColor("Version Control", 'green', this);
-        this.echo("Git            * * * * -");
+        this.echo("Git                      * * * * -");
         
         printColor("Language", 'green', this);
-        this.echo("PHP            * * * * -");
-        this.echo("JavaScript     * * * * -");
-        this.echo("Shell Script   * * * * -");
-        this.echo("Python         * * * - -");
-        this.echo("C/C++          * * * - -");
+        this.echo("PHP                      * * * * -");
+        this.echo("JavaScript               * * * * -");
+        this.echo("Shell Script             * * * * -");
+        this.echo("Java                     * * * * -");
+        this.echo("Python                   * * * - -");
+        this.echo("C/C++                    * * * - -");
 
         printColor("Database", 'green', this);
-        this.echo("MySQL          * * * * -");
+        this.echo("MySQL                    * * * * -");
         
         printColor("Package Manager", 'green', this);
-        this.echo("Composer       * * * * -");
-        this.echo("Npm/Yarn       * * * * -");
+        this.echo("Composer                 * * * * -");
+        this.echo("Npm/Yarn                 * * * * -");
         
-        printColor("Open Sources CMS and LMS", 'green', this);
-        this.echo("Typo3 CMS      * * * * -");
-        this.echo("Moodle LMS     * * * * -");
-        this.echo("Wordpress CMS  * * * * -");
+        printColor("Open Sources CMS and LMS/Software", 'green', this);
+        this.echo("Typo3 CMS                * * * * -");
+        this.echo("Wordpress CMS            * * * * -");
+        this.echo("Magento                  * * * - -");
+        this.echo("Moodle LMS               * * * * -");
         
         printColor("DevOps", 'green', this);
-        this.echo("CircleCi       * * * * -");
-        this.echo("Jenkins        * * * - -");
-        this.echo("Apache         * * * * -");
-        this.echo("Nginx          * * * - -");
-        this.echo("Prometheus     * * * * -");
-        this.echo("Grafana        * * * * -");
-        this.echo("Bind DNS       * * - - -");
+        this.echo("CircleCi                 * * * * -");
+        this.echo("Jenkins                  * * * - -");
+        this.echo("Apache                   * * * * -");
+        this.echo("Nginx                    * * * - -");
+        this.echo("Varnish Proxy            * * * - -");
+        this.echo("Prometheus               * * * * -");
+        this.echo("Grafana                  * * * * -");
+        this.echo("Bind DNS                 * * - - -");
         
         printColor("Mobile", 'green', this);
-        this.echo("React Native   * * - - -");
+        this.echo("React Native             * * - - -");
+
+        printColor("Frontend", 'green', this);
+        this.echo("Jquery                   * * * * -");
+        this.echo("Bootstrap                * * * * -");
+        this.echo("Gulp                     * * * * -");
         
         printColor("Others", 'green', this);
-        this.echo("English        * * * * -");
-        this.echo("Node.js        * * * - -");
-        this.echo("GraphQL        * * - - -");
-    }
+        this.echo("English                  * * * * -");
+        this.echo("Google Analytics         * * * * -");
+        this.echo("Node.js                  * * * - -");
+        this.echo("GraphQL                  * * - - -");
+    } else
     
     if (command == 'experience' || command == 'e') {
         printColor("Convergence Works", 'purple', this);
-        this.echo("Backend Developer with emphasis on development of news portals using typo3");
+        this.echo("Backend Developer with emphasis on development of news portals using Typo3 CMS");
         printColor("Start Interativa", 'blue', this);
         this.echo("Systems developer like Start Post and Events for social network management and events control");
         printColor("UFBA", 'green', this);
         this.echo("Developer of the first plugin of the Federal University of Bahia for the Moodle platform, where students can learn to program within the Moodle environment");
-    }
+    } else
     
     if (command == 'contact' || command == 'c') {
         printColor("Vitor Marcelino Contacts", 'yellow', this);
@@ -90,6 +101,14 @@ $('body').terminal(function(command, term) {
         this.echo("Phone: +5575991270468");
         this.echo("Location: Salvador, BA - Brazil");
 
+    } else
+    
+    if(command == 'pwd') {
+        this.echo("vitormarcelino.com.br");
+    } 
+    
+    else {
+        this.echo(command + " not found, type help to more information");
     }
-}, { prompt: '$ ', greetings: helpMessage
+}, { prompt: 'guest@vitormarcelino $ ', greetings: helpMessage
 });
